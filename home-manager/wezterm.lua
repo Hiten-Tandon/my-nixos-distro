6bfdf385da224abc56627a7b831b671cf7637370
Hiten Tandon <tandonhiten@gamil.com>
2024-11-22 12:00:31 +0530

    Feat: More wezterm config

--- home-manager/wezterm.lua
+++ home-manager/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 config.term = "wezterm"
 config.font = wezterm.font_with_fallback({ "JetBrains Mono Nerd Font" })
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.45
 config.font_size = 15
 config.enable_wayland = true
 return config
