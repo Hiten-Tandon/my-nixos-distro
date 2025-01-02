37a301baa4a8340814965b074ded76eaab5c1b85
Hiten Tandon <tandonhiten@gamil.com>
2025-01-03 02:26:40 +0530

    Feat: Updated stuff

--- home-manager/nvim-config/lua/config/keymaps.lua
+++ home-manager/nvim-config/lua/config/keymaps.lua
@@ -1,13 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
-vim.keymap.set(
-  { "n", "v" },
-  "<leader>ks",
-  require("keep-it-secret").toggle,
-  { noremap = true, silent = true, desc = "Toggle secret" }
-)
 vim.keymap.set("n", "<C-w>m", "<Cmd>WindowsMaximize<CR>")
 vim.keymap.set("n", "<C-w>_", "<Cmd>WindowsMaximizeVertically<CR>")
 vim.keymap.set("n", "<C-w>|", "<Cmd>WindowsMaximizeHorizontally<CR>")
--- home-manager/nvim-config/lua/plugins/plugin_list.lua
+++ home-manager/nvim-config/lua/plugins/plugin_list.lua
@@ -22,13 +22,6 @@ return {
   },
   { "cdmill/focus.nvim", opts = {} },
   { "meznaric/key-analyzer.nvim", opts = {} },
-  {
-    "roberte777/keep-it-secret.nvim",
-    opts = {
-      wildcards = { ".*(.env)$", ".*(.secret)$" },
-      enabled = true,
-    },
-  },
   { "ShinKage/idris2-nvim", dependencies = { "neovim/nvim-lspconfig", "MunifTanjim/nui.nvim" }, opts = {} },
   {
     "arminveres/md-pdf.nvim",
@@ -57,5 +50,43 @@ return {
       vim.o.equalalways = false
       require("windows").setup()
     end,
+  },
+  {
+    "Prometheus1400/markdown-latex-render.nvim",
+    dependencies = { "3rd/image.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+  {
+    "philosofonusus/ecolog.nvim",
+    keys = {
+      { "<leader>ge", "<cmd>EcologGoto<cr>", desc = "Go to env file" },
+      { "<leader>ep", "<cmd>EcologPeek<cr>", desc = "Ecolog peek variable" },
+      { "<leader>es", "<cmd>EcologSelect<cr>", desc = "Switch env file" },
+    },
+    -- Lazy loading is done internally
+    lazy = false,
+    opts = {
+      integrations = {
+        blink_cmp = true,
+      },
+      -- Enables shelter mode for sensitive values
+      shelter = {
+        configuration = {
+          partial_mode = false, -- false by default, disables partial mode, for more control check out shelter partial mode
+          mask_char = "*", -- Character used for masking
+        },
+        modules = {
+          cmp = true, -- Mask values in completion
+          peek = false, -- Mask values in peek view
+          files = false, -- Mask values in files
+          telescope = false, -- Mask values in telescope
+          fzf = false, -- Mask values in fzf picker
+        },
+      },
+      -- true by default, enables built-in types (database_url, url, etc.)
+      types = true,
+      path = vim.fn.getcwd(), -- Path to search for .env files
+      preferred_environment = "development", -- Optional: prioritize specific env files
+    },
   },
 }
