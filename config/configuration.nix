1bf78b3658641e7a47ab47c0583b01a13dadb0e4
Hiten Tandon <tandonhiten@gamil.com>
2026-02-12 03:57:17 +0530

    Updates

--- config/configuration.nix
+++ config/configuration.nix
@@ -145,7 +145,7 @@
 
   programs = {
     steam = {
-      enable = false;
+      enable = true;
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
