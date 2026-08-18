4d91c0a5dcb2b17405e1f2b85a43efaffa984455
Hiten Tandon <tandonhiten@gamil.com>
2026-08-18 14:53:49 +0530

    Updates

--- config/configuration.nix
+++ config/configuration.nix
@@ -104,7 +104,12 @@
     printing.enable = false;
     pipewire = {
       enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
       pulse.enable = true;
+      jack.enable = true;
     };
     libinput.enable = true;
     openssh.enable = true;
